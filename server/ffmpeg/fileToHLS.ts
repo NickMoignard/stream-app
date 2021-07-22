cd6e76a3177ae31d8eb5b52ca3aeec4528aa63ab
Nick Moignard <nicklmoignard@gmail.com>
2021-07-22 18:17:21 +1000

    working!

---
+++ server/ffmpeg/fileToHLS.ts
@@ -0,0 +1,64 @@
+import ffmpeg from "fluent-ffmpeg"
+let stream_key = "test"
+
+const renditions = [
+  {res: '426x240', vb: '400k', ab: '64k'},
+  {res: '640x360', vb: '800k', ab: '96k'},
+  {res: '842x480', vb: '1400k', ab: '128k'},
+  {res: '1280x720', vb: '2800k', ab: '128k'},
+  {res: '1920x1080', vb: '5000k', ab: '192k'},
+]
+
+const segment_target_duration = 4  // try to create a new segment every X seconds
+const MAX_BITRATE_RATIO = 1.07  // maximum accepted bitrate fluctuations
+const RATE_MONITOR_BUFFER_RATIO = 1.5 // maximum buffer size between bitrate conformance checks
+const AUDIO_RATE = 4800
+
+const source = `rtmp://127.0.0.1:1935/live/${stream_key}`
+const target = `${stream_key}`
+const media_root = '../../media/live'
+// - TODO: MEDIA ROOT
+// - TODO: fs.mkdir
+
+
+// - TODO: Probe
+// key_frames_interval="$(echo `ffprobe ${source} 2>&1 | grep -oE '[[:digit:]]+(.[[:digit:]]+)? fps' | grep -oE '[[:digit:]]+(.[[:digit:]]+)?'`*2 | bc || echo '')"
+// key_frames_interval=${key_frames_interval:-50}
+// key_frames_interval=$(echo `printf "%.1f\n" $(bc -l <<<"$key_frames_interval/10")`*10 | bc) # round
+// key_frames_interval=${key_frames_interval%.*} # truncate to integer
+const key_frames_interval = 48
+
+let static_params = "-c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0"
+static_params += ` -g ${key_frames_interval} -keyint_min ${key_frames_interval} -hls_time ${segment_target_duration}`
+static_params += " -hls_playlist_type vod"
+
+const misc_params = "-hide_banner -y"
+let master_playlist = '#EXTM3U\n#EXT-X-VERSION:3\n'
+
+let cmd = ""
+renditions.forEach((r) => {
+
+  const width =  r.res.split('x')[0]
+  const height = r.res.split('x')[1]
+  const maxrate = `${parseInt(r.vb.replace('k','')) * MAX_BITRATE_RATIO}k`
+  const bufsize = `${parseInt(r.vb.replace('k','')) * RATE_MONITOR_BUFFER_RATIO}k`
+  const bandwidth = `${r.vb.replace('k','')}000`
+  const name = `${height}p`
+  
+  cmd += ` ${static_params} -vf scale=w=${width}:h=${height}:force_original_aspect_ratio=decrease`
+  cmd += ` -b:v ${r.vb} -maxrate ${maxrate} -bufsize ${bufsize} -b:a ${AUDIO_RATE}`
+  cmd += ` -hls_segment_filename ${target}/${name}_%03d.ts ${target}/${name}.m3u8`
+  
+//   # add rendition entry in the master playlist
+  master_playlist += `#EXT-X-STREAM-INF:BANDWIDTH=${bandwidth},RESOLUTION=${r.res}\n${name}.m3u8` + "\n"
+})
+
+// # start conversion
+console.log(`Executing command:\nffmpeg ${misc_params} -i ${source} ${cmd}`)
+// ffmpeg ${misc_params} -i ${source} ${cmd}
+
+// # create master playlist file
+// echo -e "${master_playlist}" > ${target}/playlist.m3u8
+
+
+// echo "Done - encoded HLS is at ${target}/"
